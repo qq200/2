bd440ca8eaf3ddbe501186cd8de278475d3aa2b2
qq200 <43753261+qq200@users.noreply.github.com>
2019-03-01 20:00:09 +0200

    Add files via upload

---
+++ carusel-Dom.js
@@ -0,0 +1,43 @@
+var slides=[    //masiv din obiecte
+    { //index0
+        title:"Buy an",
+        src:"Images-Dom/aple-device.jpeg"
+
+    },
+    { //index1
+        title:"Free vocation",
+        src:"Images-Dom/bridge.jpeg"
+    },
+    { //index2
+        title:"Women meditation",
+        src:"Images-Dom/girls-river.jpeg",
+        // buton_text: url;
+    },
+];
+
+//logic
+function showSlide(efect,n){  //conecteaza patratele imaginii
+   //cautal divul
+   let div_slide=document.querySelector('#carusel .slide');
+    console.log(div_slide);
+   for(var row=0; row<6; row++){
+
+        for(var col=0; col<8; col++){
+        let x=-100*col;
+        let y=-100*row;
+        let d=Math.random()/2;
+        // let efect= 'flipInX';
+        let src=slides[n].src;
+        let html=`<div style="background-image: url('${src}'); background-position: ${x}px ${y}px; animation-delay:${d}s;" class="box ${efect} animated">
+        </div>`;
+        div_slide.innerHTML+=html;
+        }
+    }
+
+
+}
+showSlide('flip',2);
+
+
+// titlul fiecarui element sa apara in h1
+//un buton a cu url
