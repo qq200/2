53656324819c586de18b1c86554abd5f2f651385
qq200 <43753261+qq200@users.noreply.github.com>
2019-03-13 20:01:24 +0200

    Add files via upload

---
+++ chat-json.js
@@ -0,0 +1,53 @@
+//sablonul mesajului
+
+class Message{
+    constructor(text,author){
+        this.text=text;
+        this.author=author;
+        this.date=new Date();// current date time
+    }
+}
+///////////////////////////////
+var messages=[
+
+];
+function addMessage(){
+    var input=document.querySelector("#chat .input input");
+    //2) validation:
+    // mesage>=2 si nu include no space
+    var m=new Message(input.value, "user");
+        messages.push(m); // adauga in masiv la urma m
+    // alert(input.value);
+    localStorage.setItem("messages", JSON.stringify(messages));
+    // console.log(messages)
+    input.value="";
+    showMesseges();
+}
+function loadMesages(){
+    messages=JSON.parse(localStorage.getItem('messages'));
+    if(messages==null){
+        messages=[];
+    }
+    showMesseges();
+
+}
+function showMesseges(){
+    var div=document.querySelector('#chat .messages');
+    div.innerHTML="";
+    for(var i in messages){
+        var html=`<div class="messages">
+        <p>${messages[i].text}<hr>
+        <small>${messages[i].author}/${messages[i].date}</small>
+        </p>
+        </div>`;
+        div.innerHTML+=html;
+    }
+}
+
+// var m=new Message("hello","osama_b");
+// console.log(m);
+
+
+
+//////de facut sa arate doar ultimile 8 mesaje if + break sau clon la ,essages
+///// 2)
